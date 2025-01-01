9969afc95b5bfe33003561d2446356aa61111af1
tito10047 <mostka.j@gmail.com>
2025-01-01 11:02:22 +0100

    Remove final keywords for support php8.1

--- src/AutocompleteTreeBuilder.php
+++ src/AutocompleteTreeBuilder.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 
 namespace AceEditorBundle;
 
-final readonly class AutocompleteTreeBuilder implements AutocompleteBuilderInterface
+final class AutocompleteTreeBuilder implements AutocompleteBuilderInterface
 {
     public function __construct(
         private readonly array $tree,
